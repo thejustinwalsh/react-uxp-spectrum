36453902faed9330b3c2154041e57939071a218c
Justin Walsh <contact.me@thejustinwalsh.com>
2021-06-20 14:18:56 -0700

    chore: publish

--- dist/Icon.d.ts
+++ dist/Icon.d.ts
@@ -2,11 +2,13 @@
 declare namespace Spectrum {
     type IconName = 'ui:AlertMedium' | 'ui:AlertSmall' | 'ui:ArrowDownSmall' | 'ui:ArrowLeftMedium' | 'ui:ArrowUpSmall' | 'ui:Asterisk' | 'ui:CheckmarkMedium' | 'ui:CheckmarkSmall' | 'ui:ChevronDownMedium' | 'ui:ChevronDownSmall' | 'ui:ChevronLeftLarge' | 'ui:ChevronLeftMedium' | 'ui:ChevronRightLarge' | 'ui:ChevronRightMedium' | 'ui:ChevronRightSmall' | 'ui:ChevronUpSmall' | 'ui:CornerTriangle' | 'ui:CrossLarge' | 'ui:CrossMedium' | 'ui:CrossSmall' | 'ui:DashSmall' | 'ui:DoubleGripper' | 'ui:FolderBreadcrumb' | 'ui:HelpMedium' | 'ui:HelpSmall' | 'ui:InfoMedium' | 'ui:InfoSmall' | 'ui:Magnifier' | 'ui:More' | 'ui:SkipLeft' | 'ui:SkipRight' | 'ui:Star' | 'ui:StarOutline' | 'ui:SuccessMedium' | 'ui:SuccessSmall' | 'ui:TripleGripper';
     type IconSize = 'xxs' | 'xs' | 's' | 'm' | 'l' | 'xl' | 'xxl';
+    type IconSlot = 'icon';
 }
 declare type Props = {
     className?: string;
     name?: Spectrum.IconName;
     size?: Spectrum.IconSize;
+    slot?: Spectrum.IconSlot;
 };
 declare global {
     namespace JSX {
@@ -16,6 +18,7 @@ declare global {
                 class?: string;
                 name?: Spectrum.IconName;
                 size?: Spectrum.IconSize;
+                slot?: Spectrum.IconSlot;
             };
         }
     }
