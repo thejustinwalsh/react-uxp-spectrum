746e1622a38347a8e1282088a8728d565b981e13
Justin Walsh <contact.me@thejustinwalsh.com>
2021-06-20 14:56:07 -0700

    fix: export remaining components

--- dist/index.js
+++ dist/index.js
@@ -8,11 +8,18 @@ import ActionButton from './ActionButton';
 import Button from './Button';
 import Checkbox from './Checkbox';
 import Divider from './Divider';
+import Dropdown from './Dropdown';
 import Icon from './Icon';
 import Link from './Link';
+import Menu from './Menu';
+import MenuDivider from './MenuDivider';
+import MenuItem from './MenuItem';
 import Progressbar from './Progressbar';
 import Radio from './Radio';
 import RadioGroup from './RadioGroup';
+import Slider from './Slider';
+import Textarea from './Textarea';
+import Textfield from './Textfield';
 const Spectrum = {
     Body,
     Detail,
@@ -22,12 +29,19 @@ const Spectrum = {
     Button,
     Checkbox,
     Divider,
+    Dropdown,
     Icon,
     Link,
+    Menu,
+    MenuDivider,
+    MenuItem,
     Progressbar,
     Radio,
     RadioGroup,
+    Slider,
+    Textarea,
+    Textfield,
 };
-export { Body, Detail, Heading, Label, ActionButton, Button, Checkbox, Divider, Icon, Link, Progressbar, Radio, RadioGroup, };
+export { Body, Detail, Heading, Label, ActionButton, Button, Checkbox, Divider, Dropdown, Icon, Link, Progressbar, Radio, RadioGroup, Slider, Textarea, Textfield, };
 export default Spectrum;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,18 @@ import ActionButton from './ActionButton';
 import Button from './Button';
 import Checkbox from './Checkbox';
 import Divider from './Divider';
+import Dropdown from './Dropdown';
 import Icon from './Icon';
 import Link from './Link';
+import Menu from './Menu';
+import MenuDivider from './MenuDivider';
+import MenuItem from './MenuItem';
 import Progressbar from './Progressbar';
 import Radio from './Radio';
 import RadioGroup from './RadioGroup';
+import Slider from './Slider';
+import Textarea from './Textarea';
+import Textfield from './Textfield';
 
 const Spectrum = {
   Body,
@@ -23,11 +30,18 @@ const Spectrum = {
   Button,
   Checkbox,
   Divider,
+  Dropdown,
   Icon,
   Link,
+  Menu,
+  MenuDivider,
+  MenuItem,
   Progressbar,
   Radio,
   RadioGroup,
+  Slider,
+  Textarea,
+  Textfield,
 };
 
 export {
@@ -39,11 +53,15 @@ export {
   Button,
   Checkbox,
   Divider,
+  Dropdown,
   Icon,
   Link,
   Progressbar,
   Radio,
   RadioGroup,
+  Slider,
+  Textarea,
+  Textfield,
 };
 
 export default Spectrum;
